63effa2835dcbd70ab57c8bc30c24416f96700a2
Molnár Dominik <szepitlakinfo@gmail.com>
2026-07-27 17:34:22 +0200

    Update send-email.ts

--- api/send-email.ts
+++ api/send-email.ts
@@ -41,7 +41,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
     const { data, error } = await resend.emails.send({
       from: 'Szépít-Lak Weboldal <onboarding@resend.dev>',
-      to: 'molnardominikhun@gmail.com',
+      to: 'szepitlakinfo@gmail.com',
       replyTo: email,
       subject: `Szépít-Lak ajánlatkérés – ${name}`,
       html: `
