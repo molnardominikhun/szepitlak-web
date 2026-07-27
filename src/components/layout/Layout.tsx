84b677c433c964bea2677ecb35a67a5691ae888e
Molnár Dominik <szepitlakinfo@gmail.com>
2026-07-28 00:57:34 +0200

    végő ga4

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
@@ -6,7 +6,37 @@ import { FloatingCallbackButton } from './FloatingCallbackButton';
 import { CookieConsent } from '../ui/CookieConsent';
 
 export const Layout: React.FC = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
+
+  // Route váltáskor: görgetés az oldal tetejére + GA4 / dataLayer page_view esemény küldése
+  useEffect(() => {
+    window.scrollTo(0, 0);
+
+    const fullUrl = window.location.href;
+    const pagePath = pathname + search;
+
+    const w = window as unknown as {
+      gtag?: (...args: unknown[]) => void;
+      dataLayer?: unknown[];
+    };
+
+    if (typeof w.gtag === 'function') {
+      w.gtag('event', 'page_view', {
+        page_title: document.title,
+        page_location: fullUrl,
+        page_path: pagePath,
+      });
+    }
+
+    if (Array.isArray(w.dataLayer)) {
+      w.dataLayer.push({
+        event: 'page_view',
+        page_title: document.title,
+        page_location: fullUrl,
+        page_path: pagePath,
+      });
+    }
+  }, [pathname, search]);
 
   // Kapcsolat oldalon a lebegő gomb felesleges, mert az űrlap ott van
   const showFloating = pathname !== '/kapcsolat';
